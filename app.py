cb38d5456e1139a9eda711fc53c78641a9cb52ca
greenDev7 <zelenyandrei@gmail.com>
2021-01-05 15:29:46 +0300

    Добавлены функции и методы для работы с БД SQLite

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from flask import Flask, render_template
+import sqlite3
+
+def get_db_connection():    
+    conn = sqlite3.connect('database.db') #открываем соединение с файлом базы данных database.db
+    
+    # устанавливаем атрибут row_factory в sqlite3.Row, чтобы получить доступ 
+    # к столбцам на основе имен. Это означает, что подключение к базе 
+    # данных будет возвращать строки, которые ведут себя как обычные словари Python    
+    conn.row_factory = sqlite3.Row #
 
 app = Flask(__name__)
 
