a17cb79e886bc57a1abbadc1ff9cc73d05365039
greenDev7 <zelenyandrei@gmail.com>
2021-01-05 17:07:24 +0300

    Добавлены функции для получения поста и передачи его шаблону  post.html

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import sqlite3
+from werkzeug.exceptions import abort
 
 def get_db_connection():    
     conn = sqlite3.connect('database.db') #Открываем соединение с файлом базы данных database.db
@@ -9,6 +10,15 @@ def get_db_connection():
     # данных будет возвращать строки, которые ведут себя как обычные словари Python    
     conn.row_factory = sqlite3.Row
     return conn
+    
+def get_post(post_id):
+    """Возвращает post по его id"""
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+    if post is None: # Если такого поста не существует
+       abort(404)
+    return post
 
 app = Flask(__name__)
 
@@ -21,3 +31,8 @@ def index():
     # Передаем объект posts в качестве аргумента, который содержит результаты, полученные из базы данных. 
     # Это откроет доступ к постам блога в шаблоне index.html.
     return render_template('index.html', posts = posts)
+    
+@app.route('/<int:post_id>') # Правило переменной. После слеша передаем id-шник поста
+def post(post_id): # Здесь берем этот id
+    post = get_post(post_id) # и по нему достаем пост с помощью функции get_post()
+    return render_template('post.html', post = post) # Формируем html-страницу с отображением нашего поста
